1e31d65f0d53e0a05c8914a1f6d86b443d17f529
TheRedDiamond <162272484+TheRedDiamond95@users.noreply.github.com>
2024-07-12 19:57:52 +0000

    Fixed up some kinks in the code, so now everything functions as it should. That's all!

--- Python/main.py
+++ Python/main.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
